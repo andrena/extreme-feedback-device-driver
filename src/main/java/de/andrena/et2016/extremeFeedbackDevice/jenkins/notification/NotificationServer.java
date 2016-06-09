07d3815a58a62fb3c42d3b4cfd9f6de30b1c1d27
Fabian Knittel <fabian.knittel@andrena.de>
2016-06-09 12:07:14 +0200

    Notification server prototype

---
+++ src/main/java/de/andrena/et2016/extremeFeedbackDevice/jenkins/notification/NotificationServer.java
@@ -0,0 +1,48 @@
+package de.andrena.et2016.extremeFeedbackDevice.jenkins.notification;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import de.andrena.et2016.extremeFeedbackDevice.jenkins.culprit.Culprits;
+
+public class NotificationServer {
+	public static void main(String[] args) {
+		int portNumber = Integer.parseInt(args[0]);
+
+		try (ServerSocket serverSocket = new ServerSocket()) {
+			serverSocket.setReuseAddress(true);
+			serverSocket.bind(new InetSocketAddress(portNumber));
+			System.out.println("Listening on port " + portNumber + " ...");
+
+			while (true) {
+				try (Socket clientSocket = serverSocket.accept()) {
+					System.out.println(
+							"New connection from " + clientSocket.getInetAddress() + ":" + clientSocket.getPort());
+
+					ObjectMapper objectMapper = new ObjectMapper();
+					Notification notification = objectMapper.readValue(clientSocket.getInputStream(),
+							Notification.class);
+					System.out.println("Status: " + notification.getBuild()
+							.getStatus());
+					System.out.println("Full URL: " + notification.getBuild()
+							.getFullUrl());
+
+					URL culpritsUrl = new URL(notification.getBuild()
+							.getFullUrl() + "/api/json?pretty=true&tree=culprits[fullName]");
+					System.out.println("Requesting culprits from " + culpritsUrl + " ...");
+					Culprits culprits = objectMapper.readValue(culpritsUrl, Culprits.class);
+					System.out.println("Culprits: " + culprits.getFullNames());
+				} catch (IOException e) {
+					System.err.println(e.toString());
+				}
+			}
+		} catch (IOException e) {
+			System.err.println("Error listening on port " + portNumber + ": " + e.toString());
+		}
+	}
+}
