c5470847cee616efaa8aefe7b90dae85ae623df5
Fabian Knittel <fabian.knittel@andrena.de>
2016-06-09 14:46:36 +0200

    Start command sequence based on culprit's full name

--- src/main/java/de/andrena/et2016/extremeFeedbackDevice/jenkins/notification/NotificationServer.java
+++ src/main/java/de/andrena/et2016/extremeFeedbackDevice/jenkins/notification/NotificationServer.java
@@ -1,20 +1,27 @@
 package de.andrena.et2016.extremeFeedbackDevice.jenkins.notification;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URL;
 
+import javax.net.ServerSocketFactory;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import de.andrena.et2016.extremeFeedbackDevice.control.advanced.CommandSequenceExecutor;
 import de.andrena.et2016.extremeFeedbackDevice.jenkins.culprit.Culprits;
 
 public class NotificationServer {
 	public static void main(String[] args) {
 		int portNumber = Integer.parseInt(args[0]);
+		File controlSequencesDefinitionFile = new File(args[1]);
+		CommandSequenceExecutor commandSequenceExecutor = new CommandSequenceExecutor(controlSequencesDefinitionFile);
+		ServerSocketFactory serverSockerFactory = ServerSocketFactory.getDefault();
 
-		try (ServerSocket serverSocket = new ServerSocket()) {
+		try (ServerSocket serverSocket = serverSockerFactory.createServerSocket()) {
 			serverSocket.setReuseAddress(true);
 			serverSocket.bind(new InetSocketAddress(portNumber));
 			System.out.println("Listening on port " + portNumber + " ...");
@@ -27,16 +34,25 @@ public class NotificationServer {
 					ObjectMapper objectMapper = new ObjectMapper();
 					Notification notification = objectMapper.readValue(clientSocket.getInputStream(),
 							Notification.class);
-					System.out.println("Status: " + notification.getBuild()
-							.getStatus());
+					String status = notification.getBuild()
+							.getStatus();
+					System.out.println("Status: " + status);
 					System.out.println("Full URL: " + notification.getBuild()
 							.getFullUrl());
 
+					if ("SUCCESS".equals(status) || "ABORTED".equals(status)) {
+						System.out.println("Skipping retaliation");
+						continue;
+					}
 					URL culpritsUrl = new URL(notification.getBuild()
 							.getFullUrl() + "/api/json?pretty=true&tree=culprits[fullName]");
 					System.out.println("Requesting culprits from " + culpritsUrl + " ...");
 					Culprits culprits = objectMapper.readValue(culpritsUrl, Culprits.class);
 					System.out.println("Culprits: " + culprits.getFullNames());
+					for (String culprit : culprits.getFullNames()) {
+						System.out.println("Retaliating against " + culprit);
+						commandSequenceExecutor.runControlSequence(culprit);
+					}
 				} catch (IOException e) {
 					System.err.println(e.toString());
 				}
